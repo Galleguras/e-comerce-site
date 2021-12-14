d4e1361ed7abaa05cd75dc61833a4e318fad1ec2
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2021-12-14 16:32:45 +0100

    cambios err

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,22 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require('dotenv').config({path: `.env.${process.env.NODE_ENV}`})
+
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-postcss`, `gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-stripe`,
+      options: {
+        objects: ['Price', 'Product'],
+        secretKey: process.env.STRIPE_SECRET_KEY,
+        downloadFiles: true,
+      },
+    },
+  ],
   siteMetadata: {
     title: 'Codigo Facilito',
     url: 'https://codigofacilito.com',
